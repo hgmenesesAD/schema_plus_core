23b7af532fa879d236078379bd9e67b924929c87
Hector G Meneses <hector.meneses@autodesk.com>
2025-03-04 12:21:59 -0500

    add returning param on exec_insert

--- lib/schema_plus/core/active_record/connection_adapters/mysql2_adapter.rb
+++ lib/schema_plus/core/active_record/connection_adapters/mysql2_adapter.rb
@@ -64,7 +64,7 @@ module SchemaPlus
 
           alias exec_without_stmt exec_query
 
-          def exec_insert(sql, name, binds, pk = nil, sequence_name = nil)
+          def exec_insert(sql, name, binds, pk = nil, sequence_name = nil, returning: nil)
             SchemaMonkey::Middleware::Query::Exec.start(connection: self, sql: sql, query_name: name, binds: binds) { |env|
               env.result = super env.sql, env.query_name, env.binds, pk, sequence_name
             }.result
